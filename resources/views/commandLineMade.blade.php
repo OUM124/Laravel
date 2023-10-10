e6129b73e45fdbfa2dbd660a16bb49ecda9ee3c7
OUM124 <mouimioumayma242@gmail.com>
2023-10-11 00:25:00 +0100

    blade if for

--- resources/views/commandLineMade.blade.php
+++ resources/views/commandLineMade.blade.php
@@ -1,1 +1,31 @@
-<h2>Welcome to Laravel</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blade test</title>
+</head>
+<body>
+    @for($i=0;$i<=100;$i++)
+        The number is : {{$i}} <br>
+    @endfor
+
+
+    <br>
+    @php
+    $k = 0;
+    $x = 1;
+    @endphp
+    
+    @if($k==0)
+    The number is equal to 0
+    @endif
+
+    <br>
+
+   
+    @unless ($x==0)
+        The number is not null
+    @endunless
+</body>
+</html>
